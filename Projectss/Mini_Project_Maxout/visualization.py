e0c8f15f62dfcb36c287dff6b30d7f1149963991
shivansh-kale <shivanshkale17@gmail.com>
2026-07-31 23:06:31 +0530

    Updated version

--- Projectss/Mini_Project_Maxout/visualization.py
+++
@@ -1,8 +0,0 @@
-
-import matplotlib.pyplot as plt
-def plot(losses,accs,names):
-    for l,n in zip(losses,names): plt.plot(l,label=n)
-    plt.legend(); plt.title('Loss'); plt.show()
-    plt.figure()
-    for a,n in zip(accs,names): plt.plot(a,label=n)
-    plt.legend(); plt.title('Accuracy'); plt.show()
